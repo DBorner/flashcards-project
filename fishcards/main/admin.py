b7ddda0c078623cf9cc6232a56f8e35b409d74fe
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2024-01-24 14:12:25 +0100

    Added user try model

--- fishcards/main/admin.py
+++ fishcards/main/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
-from main.models import FishCardSet, FishCard
+from main.models import FishCardSet, FishCard, UserTry, UserTryCard
 
 admin.site.site_header = "Fishcards Admin"
 admin.site.site_title = "Fishcards Admin Portal"
 admin.site.index_title = "Welcome to Fishcards Researcher Portal"
-admin.site.register(FishCardSet)
-admin.site.register(FishCard)
+
+
+class FishCardInline(admin.TabularInline):
+    model = FishCard
+    extra = 1
+
+
+@admin.register(FishCardSet)
+class FishCardSetAdmin(admin.ModelAdmin):
+    inlines = [FishCardInline]
+    search_fields = ["name"]
+    list_display = ("name", "description", "created_at", "updated_at")
+    list_filter = ["created_at", "updated_at"]
+
+
+@admin.register(FishCard)
+class FishCardAdmin(admin.ModelAdmin):
+    search_fields = ["question"]
+    list_display = ("question", "answer")
+    list_filter = ["fishcardset"]
+
+
+class UserTryCardInline(admin.TabularInline):
+    model = UserTryCard
+    extra = 1
+
+
+@admin.register(UserTry)
+class UserTryAdmin(admin.ModelAdmin):
+    search_fields = ["user"]
+    list_display = ("user", "fishcardset", "created_at", "updated_at")
+    list_filter = ["created_at", "updated_at"]
+    inlines = [UserTryCardInline]
--- fishcards/main/models.py
+++ fishcards/main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from datetime import datetime
 from main.utils import set_file_name, CustomRichTextUploadingField
+from django.contrib.auth.models import User
 
 
 class FishCardSet(models.Model):
@@ -25,3 +26,26 @@ class FishCard(models.Model):
         if len(self.question) > 30:
             return self.question[:30] + "..."
         return self.question
+
+
+class UserTry(models.Model):
+    fishcardset = models.ForeignKey(FishCardSet, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserTryCard(models.Model):
+    class Status(models.TextChoices):
+        NOT_ANSWERED = "not_answered", "Not Answered"
+        CORRECT = "correct", "Correct"
+        SEMI_CORRECT = "semi_correct", "Semi Correct"
+        WRONG = "wrong", "Wrong"
+
+    usertry = models.ForeignKey(UserTry, on_delete=models.CASCADE)
+    fishcard = models.ForeignKey(FishCard, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=20, choices=Status.choices, default=Status.NOT_ANSWERED
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

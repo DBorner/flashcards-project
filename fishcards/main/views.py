f2cc4cca5817e0244b0dd308c404f27ac9704102
Daniel Borner <71730916+DBorner@users.noreply.github.com>
2024-01-24 14:49:43 +0100

    Added basic home view

--- fishcards/main/views.py
+++ fishcards/main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .models import *
+
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        fish_card_sets = {}
+        for fish_card_set in FishCardSet.objects.all():
+            if FishCard.objects.filter(fishcardset=fish_card_set).exists():
+                fish_card_sets[fish_card_set] = len(
+                    FishCard.objects.filter(fishcardset=fish_card_set)
+                )
+        context["fish_card_sets"] = fish_card_sets
+        print(context["fish_card_sets"])
+        context["title"] = "Fish Cards"
+        return context
